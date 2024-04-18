5c60ec759f9262d72100984e8506be834e38cfcb
Jinx <jinxwift@gmail.com>
2024-04-18 16:49:50 +0900

    계산기 만듦(선생님이)

--- Calculator/Calculator/ContentView.swift
+++ Calculator/Calculator/ContentView.swift
@@ -8,16 +8,133 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var inputValue = "0"
+    
+    let buttons = [
+        ["7", "8", "9", "/"],
+        ["4", "5", "6", "*"],
+        ["1", "2", "3", "-"],
+        [".", "0", "C", "+"],
+        ["="]
+    ]
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text(inputValue)
+                .frame(maxWidth: .infinity, alignment: .trailing)
+                .font(.system(size: 48))
+                .padding()
+                .background(Color.gray.opacity(0.2))
+            
+            ForEach(buttons, id: \.self) { row in
+                HStack {
+                    ForEach(row, id: \.self) { buttonChar in
+                        Button(action: {
+                            buttonTapped(buttonChar)
+                        }) {
+                            Text(buttonChar)
+                                .frame(maxWidth: .infinity, maxHeight: .infinity)
+                                .font(.system(size: 48))
+                                .foregroundStyle(buttonChar == "C" ? .red : .black)
+                                .border(Color.gray, width: 1)
+                        }
+                    }
+                }
+                
+            }
         }
-        .padding()
     }
+    
+    func buttonTapped(_ button: String) {
+        if(inputValue == "0") {
+            inputValue = ""
+        }
+        
+        switch button {
+        case "=":
+            calculate()
+        case "C":
+            inputValue = "0"
+        case "+","-","*","/":
+            if ["+", "-", "*", "/"].contains(inputValue.suffix(1)) {
+                inputValue.removeLast()
+            }
+            inputValue += button
+        default:
+            inputValue += button
+        }
+    }
+    
+    //    func calculate() {
+    //        let expression = NSExpression(format: inputValue)
+    //        if let value = expression.expressionValue(with: nil, context: nil) as? Double {
+    //            inputValue = String(value)
+    //        } else {
+    //            inputValue = "Invalid input"
+    //        }
+    //    }
+    //
+    
+    func calculate() {
+        let operators: [Character] = ["/", "*", "=", "/"]
+        if operators.contains(String(inputValue.suffix(1))) {
+            inputValue = "Invalid Input"
+            return
+        }
+        
+        var numbers = [Double]()
+        var currentNumber = ""
+        var currentOperator = "+"
+        
+        for char in inputValue {
+            print("-----")
+            print(char)
+            if operators.contains(char) {
+                if let number = Double(currentNumber) {
+                    switch currentOperator {
+                    case "+":
+                        numbers.append(number)
+                    case "-":
+                        numbers.append(-number)
+                    case "*":
+                        numbers[numbers.count - 1] *= number
+                    case "/":
+                        numbers[numbers.count - 1] /= number
+                    default:
+                        break
+                    }
+                    currentNumber = ""
+                    currentOperator = String(char)
+                }
+            } else {
+                currentNumber += String(char)
+            }
+        }
+        
+        if !currentNumber.isEmpty {
+            if let number = Double(currentNumber) {
+                switch currentOperator {
+                case "+":
+                    numbers.append(number)
+                case "-":
+                    numbers.append(-number)
+                case "*":
+                    numbers[numbers.count - 1] *= number
+                case "/":
+                    numbers[numbers.count - 1] /= number
+                default:
+                    break
+                }
+            }
+        }
+        
+        inputValue = String(numbers.reduce(0,+))
+    }
+    
 }
+
+
+
 
 #Preview {
     ContentView()
